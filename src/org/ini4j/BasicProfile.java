3f08d3d27654a8174cef5ea5f1d95a012b555116
k.frechette <k.frechette@gmail.com>
2016-03-10 19:02:43 +0000

    Fixed override of Map.remove(Object, Object) to compile under Java 1.8
    git-svn-id: http://seiscode.iris.washington.edu/svn/irisini4j/trunk@5 7be08ae0-92d5-4e35-b06b-29c83816d70d

--- src/org/ini4j/BasicProfile.java
+++ src/org/ini4j/BasicProfile.java
@@ -139,11 +139,11 @@ public class BasicProfile extends CommonMultiMap<String, Profile.Section> implem
         return remove((Object) section.getName());
     }
 
-    @Override public String remove(Object sectionName, Object optionName)
+    @Override public boolean remove(Object sectionName, Object optionName)
     {
         Section sec = get(sectionName);
 
-        return (sec == null) ? null : sec.remove(optionName);
+        return (sec == null) ? false : sec.remove(optionName) != null;
     }
 
     boolean isTreeMode()
---
+++ src/org/ini4j/Version.java
@@ -0,0 +1,16 @@
+package org.ini4j;
+
+public class Version {
+	/**
+	 * Get the version string.
+	 * 
+	 * @return the version string.
+	 */
+	public static String getVersion() {
+		return "0.5.4 IRIS2016070";
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getVersion());
+	}
+}
